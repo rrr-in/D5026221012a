0b1a5223ca35805fee79f45527f8f4afd6d5c707
rrr-in <karinafilzaa@gmail.com>
2023-11-20 23:24:44 +0700

    Pertemuan 20 Nopembet 2023

--- routes/web.php
+++ routes/web.php
@@ -75,14 +75,14 @@ Route::get('/5026221012', function () {
 });
 
 Route::get('/ETS', function () {
-	return view('index');
+	return view('index-1');
 });
 
 Route::get('perkalian', 'App\Http\Controllers\DosenController@index');
 
 Route::get('show', 'App\Http\Controllers\DosenController@showBlog');
 
-Route::get('/pegawai/{nama}', 'App\Http\Controllers\DosenController@showNama'); //biasa dipakai di data yang tidak rahaisa atau umum, ex: menu
+Route::get('/pegawai-1/{nama}', 'App\Http\Controllers\DosenController@showNama'); //biasa dipakai di data yang tidak rahaisa atau umum, ex: menu
 
 
 Route::get('/formulir', 'App\Http\Controllers\DosenController@formulir');
@@ -93,6 +93,12 @@ Route::get('/blog', 'App\Http\Controllers\BlogController@home');
 Route::get('/blog/tentang', 'App\Http\Controllers\BlogController@tentang');
 Route::get('/blog/kontak', 'App\Http\Controllers\BlogController@kontak');
 
-
+//route CRUD
+Route::get('/pegawai','App\Http\Controllers\PegawaiController@index');
+Route::get('/pegawai/tambah','App\Http\Controllers\PegawaiController@tambah');
+Route::post('/pegawai/store','App\Http\Controllers\PegawaiController@store');
+Route::get('/pegawai/edit/{id}','App\Http\Controllers\PegawaiController@edit');
+Route::post('/pegawai/update','App\Http\Controllers\PegawaiController@update');
+Route::get('/pegawai/hapus/{id}','App\Http\Controllers\PegawaiController@hapus');
 
 
